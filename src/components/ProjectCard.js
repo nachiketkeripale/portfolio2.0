2c9346926b89602383ee661c1fdeb638f09c8367
Nachiket Keripale <keripalenachiket@gmail.com>
2024-03-03 15:52:59 +0530

    Mobile View CSS Fix 2.0

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -6,7 +6,7 @@ const aClassName = 'p-[5px] md:p-[10px_35px] bg-[#481afe] rounded text-[#fff] no
 export default function ProjectCard(props) {
   return (
     <div>
-         <div class="p-[10px_5px_10px_5px] md:p-[15px_10px_15px_10px] shadow-[5px_5px_5px_#f0e9e9] text-[#481afe] rounded">
+         <div class="bg-[#fff] p-[10px_5px_10px_5px] md:p-[15px_10px_15px_10px] shadow-[5px_5px_5px_#f0e9e9] text-[#481afe] rounded">
             <img class="m-[10px_auto] w-[auto] p-[10px] lg:w-[500px]  b-[1px_solid_#1f1f1f] rounded" src={props.src}/>
             <h3 className='text-center mb-[10px] md:mb-[30px] text-xl md:text-2xl font-light'>{props.name}</h3>
             <ul className='mt-[5px] md:mt-[10px] justify-center sm:gap-[2px] flex  md:justify-between align-center'>
